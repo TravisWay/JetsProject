59bab5c72baa431297fc492bf3ff42f318e82185
TravisWay <travis_sti@yahoo.com>
2017-06-09 16:37:59 -0600

    Jet classes added with 5 jets added

--- src/Jets/Menu.java
+++ src/Jets/Menu.java
@@ -1,10 +1,41 @@
 package Jets;
 
+import java.util.Scanner;
+
 public class Menu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
 
+		System.out.println("Hello, Welcome to THE HANGAR. \n\nWhat would you like to do today"
+				+ "\nEnter 1 for List Fleet"
+				+ "\nEnter 2 for View fastest jet"
+				+ "\nEnter 3 for View jet with the longest range"
+				+ "\nEnter 4 for Add a jet to Fleet"
+				+ "\nEnter 5 for Quit");
+		
+		int Menuoption = input.nextInt();
+		
+		switch (Menuoption){
+			case 1: 
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+		
+		
+		
+		
+		
+		
+		}
+		
 	}
 
 }
