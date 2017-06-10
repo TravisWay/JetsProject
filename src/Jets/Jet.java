fb52796c7d002112e76535477cbccfc73c58c824
TravisWay <travis_sti@yahoo.com>
2017-06-09 18:45:07 -0600

    Criteria Met, Need to comment

--- src/Jets/Jet.java
+++ src/Jets/Jet.java
@@ -1,46 +1,52 @@
 package Jets;
 
-public abstract class Jets {
+public class Jet {
 
 	private String model;
 	private float speed;
 	private float range;
 	private int capacity;
-	
-	
-	public Jets(String model, float speed, float range, int capacity) {
+
+	public Jet(String model, float speed, float range, int capacity) {
 		super();
-		this.model = model;
-		this.speed = speed;
-		this.range = range;
-		this.capacity = capacity;
+		setCapacity(capacity);
+		setModel(model);
+		setRange(range);
+		setSpeed(speed);
+		
 	}
+
 	public String getModel() {
 		return model;
 	}
+
 	public void setModel(String model) {
 		this.model = model;
 	}
+
 	public float getSpeed() {
 		return speed;
 	}
+
 	public void setSpeed(float speed) {
-		this.speed = speed;
+		float Mach = (float) (speed/761.21);		
+		this.speed = Mach;
 	}
+
 	public float getRange() {
 		return range;
 	}
+
 	public void setRange(float range) {
 		this.range = range;
 	}
+
 	public int getCapacity() {
 		return capacity;
 	}
+
 	public void setCapacity(int capacity) {
 		this.capacity = capacity;
 	}
-	abstract void DoMath();
-	
 
-	
 }
